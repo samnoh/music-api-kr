cf032f9e29a160ee7af8a90812b495afe44270ac
Sam Noh <samnoh21@outlook.com>
2019-09-15 16:22:56 +1200

    Simplify v1.js

--- routes/v1/v1.js
+++ routes/v1/v1.js
@@ -3,40 +3,20 @@ import * as API from './api'
 
 const router = express.Router()
 
-router.get('/', function (req, res) {
-    res.json({ code: 200, v: 'v1', status: 'OK' })
+router.get('/', (req, res) => {
+  res.json({ code: 200, v: 'v1', status: 'OK' })
 })
 
-router.get('/melon/music/chart/100', function (req, res) {
-    API.getMelonTop100(req, res)
-})
+router.get('/melon/music/chart/100', API.getMelonTop100)
+router.get('/melon/music/chart/100/exo', API.getMelonTop100Exo)
 
-router.get('/melon/music/chart/100/exo', function (req, res) {
-    API.getMelonTop100Exo(req, res)
-})
+router.get('/genie/music/chart/200', API.getGenieTop200)
+router.get('/genie/music/chart/200/exo', API.getGenieTop200Exo)
 
-router.get('/genie/music/chart/200', function (req, res) {
-    API.getGenieTop200(req, res)
-})
+router.get('/bugs/music/chart/100', API.getBugsTop100)
+router.get('/bugs/music/chart/100/exo', API.getBugsTop100Exo)
 
-router.get('/genie/music/chart/200/exo', function (req, res) {
-    API.getGenieTop200Exo(req, res)
-})
-
-router.get('/bugs/music/chart/100', function (req, res) {
-    API.getBugsTop100(req, res)
-})
-
-router.get('/bugs/music/chart/100/exo', function (req, res) {
-    API.getBugsTop100Exo(req, res)
-})
-
-router.get('/naver/music/chart/100', function (req, res) {
-    API.getNaverTop100(req, res)
-})
-
-router.get('/naver/music/chart/100/exo', function (req, res) {
-    API.getNaverTop100Exo(req, res)
-})
+router.get('/naver/music/chart/100', API.getNaverTop100)
+router.get('/naver/music/chart/100/exo', API.getNaverTop100Exo)
 
 export default router
